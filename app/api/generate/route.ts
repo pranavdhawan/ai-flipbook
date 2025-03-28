63b6a40db8dce6cf98f96b173500c832a1df27f4
Pranav Dhawan <dhawan.pranav8@gmail.com>
2025-03-29 01:33:37 +0530

    dumb fix

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,13 +1,22 @@
-import { GoogleGenerativeAI } from "@google/generai";
+const { GoogleGenAI } = require("@google/genai");
 import { NextResponse } from "next/server";
 
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-const ai = new GoogleGenerativeAI(GEMINI_API_KEY);
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY; // Store in .env file
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
 export async function POST(request: Request) {
+    const contents = `Create an image of a scene described in a book. 
+    Visualize this scene in a highly detailed, realistic style.
+    Don't include any text in the image. Give me only an image describing the scene.
+                    `;
+
     const { prompt } = await request.json();
+    if (!prompt) {
+        prompt.push(contents);
+    }
 
     try {
+        // Set responseModalities to include "Image" so the model can generate  an image
         const response = await ai.models.generateContent({
             model: 'gemini-2.0-flash-exp-image-generation',
             contents: prompt,
@@ -15,8 +24,8 @@ export async function POST(request: Request) {
                 responseModalities: ['Text', 'Image']
             },
         });
-
         for (const part of response.candidates[0].content.parts) {
+            // Based on the part type, either show the text or save the image
             if (part.inlineData) {
                 const imageData = part.inlineData.data;
                 return NextResponse.json({
@@ -30,4 +39,3 @@ export async function POST(request: Request) {
         return NextResponse.json({ error: 'Failed to generate image' }, { status: 500 });
     }
 }
-
